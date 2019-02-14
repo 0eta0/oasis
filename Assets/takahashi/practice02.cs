c3bee5145aeac3610a33cffec731a1dd3fcc3394
高橋　宏紀 <hirotaka@yairilab.net>
2019-02-14 16:14:42 +0900

    ちょっとエラーの修正

--- Assets/takahashi/practice02.cs
+++ Assets/takahashi/practice02.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class practice01 : MonoBehaviour {
+public class practice02 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("practice");
+		
 	}
 	
 	// Update is called once per frame
